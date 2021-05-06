80debcf98ee8f659ae44bedfe094ee80dea1ddf2
mauronaca <mnacachian8@gmail.com>
2021-05-06 18:00:32 -0300

    primera version para sumar

--- ej1/ej1.py
+++ ej1/ej1.py
@@ -1,20 +1,20 @@
+
 from nmigen import *
 from nmigen_cocotb import run
 import cocotb
 from cocotb.triggers import RisingEdge, Timer
-from cocotb.clocl import Clock
+from cocotb.clock import Clock
 from random import getrandbits
 
+
 class Stream(Record):
-	def __init__(self, width, **kwargs):
-		# Define los buses:
-		Record.__init__(self, [('data', width), ('valid', 1), ('ready', 1)], **kwargs)
+    def __init__(self, width, **kwargs):
+        Record.__init__(self, [('data', width), ('valid', 1), ('ready', 1)], **kwargs)
 
-	# Devuelve 1 si valid y ready estan seteados.
-	def accepted(self):
-		return self.valid & self.ready
+    def accepted(self):
+        return self.valid & self.ready
 
-	class Driver:
+    class Driver:
         def __init__(self, clk, dut, prefix):
             self.clk = clk
             self.data = getattr(dut, prefix + 'data')
@@ -41,9 +41,77 @@ class Stream(Record):
             self.ready <= 0
             return data
 
-class Adder(Elaboratable):
 
-	def __init__(self, width):
-		self.a = Stream(width, name = 'a')
-		self.b = Stream(width, name = 'b')
-		self.r = Stream(width, name = 'r')
+class Adder(Elaboratable):
+    def __init__(self, width):
+        self.a = Stream(width, name='a')
+        self.r = Stream(width, name='r')
+        self.b = Stream(width, name='b')
+
+    def elaborate(self, platform):
+        m = Module()
+        sync = m.d.sync
+        comb = m.d.comb
+
+
+        with m.If(self.r.accepted()):
+            sync += self.r.valid.eq(0)
+
+        comb += self.a.ready.eq((~self.r.valid) | (self.r.accepted()))   
+        comb += self.b.ready.eq((~self.r.valid) | (self.r.accepted()))   
+
+        with m.If(self.a.accepted() & self.b.accepted()):
+            sync += [
+                self.r.valid.eq(1),
+                self.r.data.eq(self.a.data + self.b.data)
+            ]
+
+        #sync += self.r.data.eq(~self.r.data)
+        #sync += self.a.data.eq(1)
+        return m
+
+
+async def init_test(dut):
+    cocotb.fork(Clock(dut.clk, 2, 'ns').start()) # 500 millones p/seg
+    dut.rst <= 1
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+    dut.rst <= 0
+
+
+@cocotb.test()
+async def burst(dut):
+    # Si envia datos entonces valid = 1, cuando termina se pone en 0, y mientras que ready sea 1 va a enviarlos, si es 0 se queda eseperando.
+    # Si recibe datos ready se setea, mientras que valid = 1 los recibe en c/ flanco.
+    await Timer(1,'ns')
+
+    await init_test(dut)
+
+    stream_input_a = Stream.Driver(dut.clk, dut, 'a__')
+    stream_input_b = Stream.Driver(dut.clk, dut, 'b__')
+    stream_output = Stream.Driver(dut.clk, dut, 'r__')
+
+    N = 100
+    width = len(dut.a__data)
+    mask = int('1' * width, 2)
+    data_a = [_ for _ in range(N)]
+    data_b = [_ for _ in range(N)]
+    cocotb.fork(stream_input_b.send(data_b))
+    cocotb.fork(stream_input_a.send(data_a))
+
+    #await Timer(N * 10, 'ns')
+    recved = await stream_output.recv(N)
+
+
+if __name__ == '__main__':
+    core = Adder(5)
+    run(
+        core, 'ej1',
+        ports=
+        [
+            *list(core.a.fields.values()),
+            *list(core.r.fields.values()),
+            *list(core.b.fields.values())
+        ],
+        vcd_file='adder.vcd'
+    )   
